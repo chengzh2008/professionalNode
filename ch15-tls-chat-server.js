ec2f906ee29f27db0bf8b9e74edacc409cda356e
chengzh2008 <chengzh2008@gmail.com>
2015-01-04 11:28:04 -0800

    chapter 17 testing module and application

--- ch15-tls-chat-server.js
+++ ch15-tls-chat-server.js
@@ -4,28 +4,31 @@ var port = 4001;
 var clients = [];
 var options = {
     key: fs.readFileSync('./chat_server/server_key.pem'),
-    cert: fs.readFileSync('./chat_server/server_cert.pem')
+    cert: fs.readFileSync('./chat_server/server_cert.pem'),
+    //ca: [fs.readFileSync('./chat_client/client_cert.pem')],
+    requestCert: false,
+    rejectUnauthorized: false
 };
 function distribute(from, data) {
     var socket = from.socket;
-    clients.forEach(function(client){
-        if(client !== from) {
+    clients.forEach(function (client) {
+        if (client !== from) {
             client.write(socket.remoteAddress + ":" + socket.remotePort + " said:" + data);
         }
     });
 }
 
-var server = tls.createServer(options, function(client){
+var server = tls.createServer(options, function (client) {
     clients.push(client);
-    client.on('data', function(data){
+    client.on('data', function (data) {
         distribute(client, data);
     });
-    client.on('close', function(){
+    client.on('close', function () {
         console.log('closed connection');
         clients.splice(clients.indexOf(client), 1);
     });
 });
 
-server.listen(port, function(){
+server.listen(port, function () {
     console.log('listening on port', server.address().port);
 })
